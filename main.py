a8b730fa04679e52ac733701dca70272a8b545e6
mathWay311 <matvej0311@gmail.com>
2023-06-03 20:56:02 +0700

    test commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     tk.set_default_color_theme("custom_theme.json")
     root = tk.CTk()
     root.title("G7 Airlines - Авторизация")
-    root.geometry("1600x900")
+    root.geometry("440x10")
     root.resizable(True,True)
 
     winc = wincontroller.WinController(root)
